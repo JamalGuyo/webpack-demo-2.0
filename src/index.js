e4996689cdaca081d0471d340c6fde5542345b6e
JamalGuyo <jamalguyo08@gmail.com>
2019-10-22 10:13:37 +0300

    install and configure css file to be bundled as a js file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
+import './index.css';
 import { salamu } from './app/app';
 console.log(salamu);
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,17 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   devtool: 'none',
-  entry: '.src/index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   }
 };
