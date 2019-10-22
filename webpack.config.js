4e55527b31016f3d879be75a6477d77e55d7d4b4
JamalGuyo <jamalguyo08@gmail.com>
2019-10-22 11:23:34 +0300

    minify html using html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,15 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            minimize: true
+          }
+        }
+      },
+      {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader']
       }
